fefc8ffdce27b43fdbb58b119e5734a50d5bd385
andersh <anders.haal@ingby.com>
2020-08-09 17:53:04 +0200

    Update the bucket size to be more aligend to actual response time.

--- aci-connection.go
+++ aci-connection.go
@@ -32,7 +32,7 @@ import (
 var responseTime = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    MetricsPrefix + "response_time_from_apic",
 	Help:    "Histogram of the time (in seconds) each request took to complete.",
-	Buckets: []float64{0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 3.0, 10, 20, 30},
+	Buckets: []float64{0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0},
 },
 	[]string{"class", "method", "status"},
 )
