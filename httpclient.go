810e4a470ecab1701079ff4b291e2ad4f4503558
andersh <anders.haal@ingby.com>
2023-12-02 17:08:50 +0100

    feat: issue #42 including additional metrics for auth refresh login sessions using json api for login api

--- httpclient.go
+++ httpclient.go
@@ -9,7 +9,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
-// Copyright 2020 Opsdis AB
+// Copyright 2020-2023 Opsdis
 
 package main
 
@@ -21,60 +21,27 @@ import (
 	"time"
 )
 
-// HTTPClient used for retrieve data from a HTTP based api
+// HTTPClient used for retrieve data from an HTTP based api
 type HTTPClient struct {
 	InsecureHTTPS       bool
 	Timeout             int
 	Keepalive           int
 	Tlshandshaketimeout int
-	cookieJar           http.CookieJar
-}
-
-// GetJar return the the client jar
-func (c HTTPClient) GetJar() http.CookieJar {
-	return c.cookieJar
 }
 
 // GetClient return a http client
 func (c HTTPClient) GetClient() *http.Client {
 
-	//if c.Timeout == 0 {
-	//	c.Timeout = 3
-	//}
-	//if c.Keepalive == 0 {
-	//	c.Keepalive = 10
-	//}
-	//if c.Tlshandshaketimeout == 0 {
-	//	c.Tlshandshaketimeout = 10
-	//}
-	//
 	rootCAs, _ := x509.SystemCertPool()
 	if rootCAs == nil {
 		rootCAs = x509.NewCertPool()
 	}
-	/*
-		/ Read in the cert file
 
-		const (
-			localCertFile = "/usr/local/internal-ca/ca.crt"
-		)
-
-			certs, err := ioutil.ReadFile(localCertFile)
-		if err != nil {
-			log.Fatalf("Failed to append %q to RootCAs: %v", localCertFile, err)
-		}
-
-		/ Append our cert to the system pool
-		if ok := rootCAs.AppendCertsFromPEM(certs); !ok {
-			log.Println("No certs appended, using system certs only")
-		}
-	*/
-	//
 	var client = &http.Client{
 		Timeout: time.Duration(c.Timeout) * time.Second,
 		Transport: &http.Transport{
 			DialContext: (&net.Dialer{
-				//Timeout:   time.Duration(c.Timeout) * time.Second,
+				Timeout:   time.Duration(c.Timeout) * time.Second,
 				KeepAlive: time.Duration(c.Keepalive) * time.Second,
 			}).DialContext,
 			//TLSHandshakeTimeout: time.Duration(c.Tlshandshaketimeout) * time.Second,
@@ -85,7 +52,6 @@ func (c HTTPClient) GetClient() *http.Client {
 			//ExpectContinueTimeout: 4 * time.Second,
 			//ResponseHeaderTimeout: 3 * time.Second,
 		},
-		Jar: c.cookieJar,
 	}
 	return client
 }
