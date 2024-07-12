0417c376a1b8d5131f7758d7ea35a87c1aa89b1c
andersh <anders.haal@ingby.com>
2024-07-12 17:27:36 +0200

    fix: license text

--- httpclient.go
+++ httpclient.go
@@ -8,8 +8,6 @@
 // GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//
-// Copyright 2020-2023 Opsdis
 
 package main
 
